fea0db861f20580b001c729c30561e7c32155311
Freya Connor <Freya.Connor@softwire.com>
2023-12-19 14:28:28 +0000

    Add items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from todo_app.data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config
@@ -7,7 +7,24 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        title = request.form['add-items']
+        add_item(title)
+        # Redirect to the index page to avoid form resubmission on page refresh
+        return redirect(url_for('index'))
+
+    # If it's a GET request or after processing the POST request, render the index page
     to_do_items = get_items()
-    return render_template('index.html', to_do_items = to_do_items)
+    return render_template('index.html', to_do_items=to_do_items)
+
+#@app.route('/form', methods = ['POST', 'GET'])
+#def form():
+    #title = request.form['add-items']
+    #add_item(title)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
