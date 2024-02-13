4acd38522b740b11aec296e2bfa44fceeae7fe19
Freya <freya.connor@softwire.com>
2024-02-13 14:19:51 +0000

    split into two methods

--- todo_app/app.py
+++ todo_app/app.py
@@ -7,17 +7,16 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/', methods = ['GET'])
 def index():
-    if request.method == 'POST':
-        title = request.form['add-items']
-        add_item(title)
-        # Redirect to the index page to avoid form resubmission on page refresh
-        return redirect(url_for('index'))
-
-    # If it's a GET request or after processing the POST request, render the index page
     to_do_items = get_items()
     return render_template('index.html', to_do_items=to_do_items)
+
+@app.route('/', methods=['POST'])
+def new_item():
+    title = request.form['add-items']
+    add_item(title)
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
