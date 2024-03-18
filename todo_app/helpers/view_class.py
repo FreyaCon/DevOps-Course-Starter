2e053b1729caefd4213c7270008a7bc188abdc41
Freya <freya.connor@softwire.com>
2024-03-18 12:51:36 +0000

    exercise 3: add to do and done properties

--- todo_app/helpers/view_class.py
+++ todo_app/helpers/view_class.py
@@ -1,5 +1,7 @@
+from todo_app.helpers.item_class_helper import Card_Item
+
 class ViewModel:
-    def __init__(self, items):
+    def __init__(self, items: list[Card_Item]):
         self._items = items
  
     @property
@@ -8,4 +10,16 @@ class ViewModel:
 
     @property
     def done_items(self):
-        return ["test"]
+        done_items =[]
+        for item in self.items:
+            if item.status == "Done":
+                done_items.append(item)
+        return done_items
+    
+    @property
+    def to_do_items(self):
+        to_do_items =[]
+        for item in self.items:
+            if item.status == "To do":
+                to_do_items.append(item)
+        return to_do_items
--- todo_app/tests/test_view_model.py
+++ todo_app/tests/test_view_model.py
@@ -3,8 +3,8 @@ from todo_app.helpers.item_class_helper import Card_Item
 
 def test_view_model_done_property():
     # Arrange
-    card = Card_Item(1, 123, "something to do", "something to do", "TO DO")
-    card2 = Card_Item(2, 1234, "something to do aswell", "something to do", "DONE")
+    card = Card_Item(1, 123, "something to do", "something to do", "To do")
+    card2 = Card_Item(2, 1234, "something to do aswell", "something to do", "Done")
     model = ViewModel([card, card2])
 
     # Act
@@ -12,4 +12,15 @@ def test_view_model_done_property():
 
     # Assert
     assert done_items == [card2]
-    
+
+def test_view_model_todo_property():
+    # Arrange
+    card = Card_Item(1, 123, "something to do", "something to do", "To do")
+    card2 = Card_Item(2, 1234, "something to do aswell", "something to do", "Done")
+    model = ViewModel([card, card2])
+
+    # Act
+    to_do_items = model.to_do_items
+
+    # Assert
+    assert to_do_items == [card]
